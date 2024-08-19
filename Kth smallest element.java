27788dea71b71a557e0c78dc4b87d2061f9f19b2
vysuvysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-08-19 15:50:44 +0530

    Add files via upload

---
+++ Kth smallest element.java
@@ -0,0 +1,11 @@
+ PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int num : arr) {
+            maxHeap.add(num);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        
+ -       The root of the heap is the k-th smallest element
+        return maxHeap.peek();
