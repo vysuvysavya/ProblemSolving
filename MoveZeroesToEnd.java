a6f5bc797f42488c301e7ee8c749bc5bd216b031
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-08-23 23:01:24 +0530

    Update MoveZeroesToEnd.java

--- MoveZeroesToEnd.java
+++ MoveZeroesToEnd.java
@@ -15,6 +15,15 @@ class Solution {
         }
     }
 }
+---------------------------------------------------------------
+int temp , j = 0;
+        for (int i = 0 ;i < nums.length ;i++){
+            if (nums[i]!=0 ){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for (int i = j; i < nums.length; i++) nums[i]=0;
 
 
 nums =
