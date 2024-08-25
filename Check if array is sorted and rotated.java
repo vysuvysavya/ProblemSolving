1d6196bdc736b82150eeff13d8cbad3e3a868c72
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-08-25 15:37:35 +0530

    Update Check if array is sorted and rotated.java

--- Check if array is sorted and rotated.java
+++ Check if array is sorted and rotated.java
@@ -15,6 +15,18 @@ class Solution {
         return true;
     }
 }
+---------------------------
+class Solution {
+    public boolean check(int[] nums) {
+        int n = nums.length, count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+}
 
 ---------
 input
