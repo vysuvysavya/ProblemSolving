e2bc1b7fc128374f00c08e285dcb1cd3c2ef2db3
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-08-28 21:51:13 +0530

    Create Sort an array of 0's 1's and 2's.py

---
+++ Sort an array of 0's 1's and 2's.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortColors(self, arr: List[int]) -> None:
+        low = 0
+        mid = 0
+        high = len(arr) - 1
+
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif arr[mid] == 1:
+                mid += 1
+            else:
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+
+==================================================================================
+
+arr = [0, 2, 1, 2, 0, 1]
+Output:
+After sorting:
+0 0 1 1 2 2
+
+based on the value of the mid pointer.
+
+The steps will be the following:
+
+First, we will run a loop that will continue until mid <= high.
+There can be three different values of mid pointer i.e. arr[mid]
+If arr[mid] == 0, we will swap arr[low] and arr[mid] and will increment both low and mid. Now the subarray from index 0 to (low-1) only contains 0.
+If arr[mid] == 1, we will just increment the mid pointer and then the index (mid-1) will point to 1 as it should according to the rules.
+If arr[mid] == 2, we will swap arr[mid] and arr[high] and will decrement high. Now the subarray from index high+1 to (n-1) only contains 2.
+In this step, we will do nothing to the mid-pointer as even after swapping, the subarray from mid to high(after decrementing high) might be unsorted. So, we will check the value of mid again in the next iteration.
