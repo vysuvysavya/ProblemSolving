156ac8e4f0249068bba4f85656b10b7f986f0ebb
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-08-29 21:58:10 +0530

    Create Implement stack using array.java

---
+++ Implement stack using array.java
@@ -0,0 +1,24 @@
+class stack {
+    int size = 10000;
+    int arr[] = new int[size];
+    int top = -1;
+    void push(int x) {
+        top++;
+        arr[top] = x;
+    }
+    int pop() {
+        int x = arr[top];
+        top--;
+        return x;
+    }
+    int top() {
+        return arr[top];
+    }
+    int size() {
+        return top + 1;
+    }
+}
+
+Time Complexity: O(N)
+
+Space Complexity: O(N)
