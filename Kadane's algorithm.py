2c7d4a0795d7efc72e9aa324ac87fbcb345a8220
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-09-06 22:54:45 +0530

    Create Kadane's algorithm.py

---
+++ Kadane's algorithm.py
@@ -0,0 +1,29 @@
+Given an integer array arr[]. Find the contiguous sub-array(containing at least one number) that has the maximum sum and return its sum.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, -2, 5]
+Output: 9
+Explanation: Max subarray sum is 9 of elements (1, 2, 3, -2, 5) which is a contiguous subarray.
+Input: arr[] = [-1, -2, -3, -4]
+Output: -1
+Explanation: Max subarray sum is -1 of element (-1)
+Input: arr[] = [5, 4, 7]
+Output: 16
+Explanation: Max subarray sum is 16 of element (5, 4, 7)
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(1)
+
+=================================================================================
+
+
+class Solution:
+    def maxSubArraySum(self, arr):
+        max_sum = arr[0]
+        current_sum = arr[0]
+        
+        for i in range(1, len(arr)):
+            current_sum = max(arr[i], current_sum + arr[i])
+            max_sum = max(max_sum, current_sum)
+        
+        return max_sum
