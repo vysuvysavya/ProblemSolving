77626aaa95dd36dd6f1e9e0f0e7e5eb7d2d0f3a0
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-09-15 13:34:43 +0530

    Update set matrix zeroes.py

--- set matrix zeroes.py
+++ set matrix zeroes.py
@@ -57,6 +57,31 @@ def zeroMatrix(matrix, n, m):
 
 ====================================================================================
 
+
+def zeroMatrix(matrix, n, m):
+    row = [0] * n  # row array
+    col = [0] * m  # col array
+
+    # Traverse the matrix:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                # mark ith index of row wih 1:
+                row[i] = 1
+
+                # mark jth index of col wih 1:
+                col[j] = 1
+
+    # Finally, mark all (i, j) as 0
+    # if row[i] or col[j] is marked with 1.
+    for i in range(n):
+        for j in range(m):
+            if row[i] or col[j]:
+                matrix[i][j] = 0
+
+    return matrix
+===================================================================
+
 Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
 
 You must do it in place.
