8a828905ee96f1505cfe8194f10ec7c669b72bbf
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-09-30 15:27:33 +0530

    Create Reverse Vowels of a String.java

---
+++ Reverse Vowels of a String.java
@@ -0,0 +1,46 @@
+Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+Example 1:
+
+Input: s = "IceCreAm"
+
+Output: "AceCreIm"
+
+Explanation:
+
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+Example 2:
+
+Input: s = "leetcode"
+
+Output: "leotcede"
+===============================================================
+class Solution {
+    public String reverseVowels(String s) {
+        char[] word = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        String vowels = "aeiouAEIOU";
+        while (start < end) {
+            while (start < end && vowels.indexOf(word[start]) == -1) {
+                start++;
+            }
+            while (start < end && vowels.indexOf(word[end]) == -1) {
+                end--;
+            }
+            char temp = word[start];
+            word[start] = word[end];
+            word[end] = temp;
+            
+            start++;
+            end--;
+        }
+        String answer = new String(word);
+        return answer;
+    }
+}
