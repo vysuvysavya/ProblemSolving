d08b8d655e38294f502743a15f13ac57950704ca
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-11-18 21:05:15 +0530

    Update SecondLargest.py

--- SecondLargest.py
+++ SecondLargest.py
@@ -14,3 +14,12 @@ class Solution:
 
 Input: arr = [12, 35, 1, 10, 34, 1]
 Output: 34
+---------------------------------------------------------
+class Solution:
+    def getSecondLargest(self, arr):
+        # Remove duplicates and sort in descending order
+        arr = list(set(arr))
+        if len(arr) < 2:
+            return -1  # Return -1 if there's no second largest
+        arr.sort(reverse=True)
+        return arr[1]  # Return the second largest element
