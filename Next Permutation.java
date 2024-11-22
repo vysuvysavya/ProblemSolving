efd2bfe43637f3c257cad570eba2aa9f2295c929
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-11-22 13:51:32 +0530

    Create Next Permutation.java

---
+++ Next Permutation.java
@@ -0,0 +1,75 @@
+Given an array of integers arr[] representing a permutation, implement the next permutation that rearranges the numbers into the lexicographically next greater permutation. If no such permutation exists, rearrange the numbers into the lowest possible order (i.e., sorted in ascending order). 
+
+Note - A permutation of an array of integers refers to a specific arrangement of its elements in a sequence or linear order.
+
+Examples:
+
+Input: arr = [2, 4, 1, 7, 5, 0]
+Output: [2, 4, 5, 0, 1, 7]
+Explanation: The next permutation of the given array is {2, 4, 5, 0, 1, 7}.
+Input: arr = [3, 2, 1]
+Output: [1, 2, 3]
+Explanation: As arr[] is the last permutation, the next permutation is the lowest one.
+Input: arr = [3, 4, 2, 5, 1]
+Output: [3, 4, 5, 1, 2]
+Explanation: The next permutation of the given array is {3, 4, 5, 1, 2}.
+====================================================================================================
+
+// Java Program to find the next permutation by 
+// generating only next
+
+import java.util.Arrays;
+
+class GfG {
+    static void nextPermutation(int[] arr) {
+        int n = arr.length;
+
+        // Find the pivot index
+        int pivot = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        // If pivot point does not exist, reverse the whole array
+        if (pivot == -1) {
+            reverse(arr, 0, n - 1);
+            return ;
+        }
+
+        // Find the element from the right that is greater than pivot
+        for (int i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                swap(arr, i, pivot);
+                break;
+            }
+        }
+
+        // Reverse the elements from pivot + 1 to the end
+        reverse(arr, pivot + 1, n - 1);
+    }
+
+    // Helper method to reverse array
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+
+    // Helper method to swap two elements
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 1, 7, 5, 0 };
+        nextPermutation(arr);
+        
+        for(int i = 0; i < arr.length; i++)
+        System.out.print(arr[i] + " ");
+    }
+}
