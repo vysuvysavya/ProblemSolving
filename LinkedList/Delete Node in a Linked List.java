9aba57ca78db1b46b5dbbb78f0fc8a1e048bed35
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2024-12-24 21:56:41 +0530

    Create Delete Node in a Linked List.java

---
+++ LinkedList/Delete Node in a Linked List.java
@@ -0,0 +1,16 @@
+You are given the node to be deleted node. You will not be given access to the first node of head.
+
+Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+
+The value of the given node should not exist in the linked list.
+The number of nodes in the linked list should decrease by one.
+All the values before node should be in the same order.
+All the values after node should be in the same order.
+
+=====
+class Solution {
+    public void deleteNode(ListNode node) {
+         node.val = node.next.val;
+         node.next = node.next.next;
+    }
+}
