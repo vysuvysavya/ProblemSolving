3b7c28003961b80d7019f87f0f9ec9674db60dd1
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2025-01-04 20:37:59 +0530

    Update 1st last occurences.py

--- 1st last occurences.py
+++ 1st last occurences.py
@@ -5,3 +5,9 @@
             if s[i] not in first_occurrence:
                 first_occurrence[s[i]] = i
             last_occurrence[s[i]] = i
+
+or simply
+
+
+first_occurrence = {ch: s.find(ch) for ch in set(s)}
+last_occurrence = {ch: s.rfind(ch) for ch in set(s)}
