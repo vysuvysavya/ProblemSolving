b94254a4f45aaa7ab900e2ffc176255eb08a491b
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2025-05-12 20:46:54 +0530

    c1

--- Closest to 0.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-arr = list(map(int, input().split()))
-
-closest = arr[0]
-for num in arr:
-    if abs(num) < abs(closest) or (abs(num) == abs(closest) and num > closest):
-        closest = num
-
-print(closest)
